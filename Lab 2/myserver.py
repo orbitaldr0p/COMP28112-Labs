8de26ff48a009b56475a7ffb3b525914672aa39d
_orbitaldrop <githubonly@proton.me>
2024-03-13 15:14:40 +0000

    commands are now triggered using /

--- Lab 2/myserver.py
+++ Lab 2/myserver.py
@@ -8,26 +8,30 @@ class myServer(Server):
 
     def __init__(self):
         super(myServer, self).__init__()
-        self.users = 0
+        self.users = set({})
+        self.userCount = 0
 
     def onStart(self):
         print("Server has started")
     
     def onConnect(self, socket):
-        self.users += 1
-        print(f"User has connected. Currently, {self.users} {'user is' if self.users == 1 else 'users are'} connected.")
+        self.userCount += 1
+        socket.send(("Welcome to the chatroom! Please give yourself a name by typing /setname <name>.").encode())
+        print(f"User has connected. Currently, {self.userCount} {'user is' if self.userCount == 1 else 'users are'} connected.")
 
     def onDisconnect(self, socket):
-        self.users -= 1
-        print(f"User has disconnected. Currently, {self.users} {'user is' if self.users == 1 else 'users are'}  connected.")
+        self.userCount -= 1
+        print(f"User has disconnected. Currently, {self.userCount} {'user is' if self.userCount == 1 else 'users are'}  connected.")
 
     def onMessage(self, socket, message):
         print("A User has sent a message")
-        message = message.split()
-        command = message[0]
-        arguments = message[1:]
+        if message[0] == "/":
+            message = message.split()
+            command = message[0].replace("/", "")
+            arguments = message[1:]
 
-        print(f"command: {command}, arguments: {arguments}")
+            print(f"command: {command}, arguments: {arguments}")
+        
         return True
 
 
