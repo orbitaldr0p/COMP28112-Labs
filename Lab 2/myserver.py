8a8f9d6fa89c35ea9e154ce77e5b5325a9beb7e6
_orbitaldrop <githubonly@proton.me>
2024-03-13 18:31:39 +0000

    format

--- Lab 2/myserver.py
+++ Lab 2/myserver.py
@@ -4,6 +4,7 @@ from ex2utils import Server
 ip = sys.argv[1]
 port = int(sys.argv[2])
 
+
 class myServer(Server):
 
     def __init__(self):
@@ -13,11 +14,17 @@ class myServer(Server):
 
     def onStart(self):
         print("Server has started")
-    
+
     def onConnect(self, socket):
         self.userCount += 1
-        socket.send(("Welcome to the chatroom! Please give yourself a name by typing /setname <name>.").encode())
-        print(f"User has connected. Currently, {self.userCount} {'user is' if self.userCount == 1 else 'users are'} connected.")
+        socket.send(
+            (
+                "Welcome to the chatroom! Please give yourself a name by typing /setname <name>."
+            ).encode()
+        )
+        print(
+            f"User has connected. Currently, {self.userCount} {'user is' if self.userCount == 1 else 'users are'} connected."
+        )
 
     def onMessage(self, socket, message):
         if message[0] == "/":
@@ -47,16 +54,17 @@ class myServer(Server):
         socket.send("Invalid command. Type /help to get a list of commands.".encode())
 
     def help(self, socket):
-        commands = "Available Commands:\n"\
-                "/setname <username>: sets your current name in the system \n"\
-                "/whisper <username> <message>: sends a message to the specified user \n"\
-                "/list: lists the users that are currently online\n"\
-                "/quit: exi3ts the system \n"\
-                "type something without using a command to send it to the entire system \n"\
-                .encode()
+        commands = (
+            "Available Commands:\n"
+            "/setname <username>: sets your current name in the system \n"
+            "/whisper <username> <message>: sends a message to the specified user \n"
+            "/list: lists the users that are currently online\n"
+            "/quit: exi3ts the system \n"
+            "type something without using a command to send it to the entire system \n".encode()
+        )
         socket.send(commands)
 
-    def setName(self,socket, arguments):
+    def setName(self, socket, arguments):
         if len(arguments) > 1:
             socket.send("Your username can't contain spaces!".encode())
         elif len(arguments) == 0:
@@ -69,10 +77,9 @@ class myServer(Server):
             else:
                 socket.send(f"Your username is set to {arguments[0]}.".encode())
                 self.users[socket] = arguments[0]
-                
 
-    def list(self,socket):
-        online = '\n'.join(list(self.users.values()))
+    def list(self, socket):
+        online = "\n".join(list(self.users.values()))
         socket.send(f"Currently online users:\n{online}".encode())
 
     def whisper(self, socket, arguments):
@@ -81,9 +88,9 @@ class myServer(Server):
     def quit(self, socket):
         socket.send("Quitting out.".encode())
         socket.close()
-        #TODO STOP socket.close() FROM BREAKING SERVER
+        # TODO STOP socket.close() FROM BREAKING SERVER
         return True
-    
+
     def postMessage(self, socket, message):
         if socket in self.users:
             msg = f"{self.users[socket]}: {message}"
@@ -102,7 +109,10 @@ class myServer(Server):
             print(quitMessage)
             del self.users[socket]
         else:
-            print(f"User has disconnected. Currently, {self.userCount} {'user is' if self.userCount == 1 else 'users are'}  connected.")
+            print(
+                f"User has disconnected. Currently, {self.userCount} {'user is' if self.userCount == 1 else 'users are'}  connected."
+            )
+
 
 server = myServer()
 
